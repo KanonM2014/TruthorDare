6a51af7cca81e49b5283af0a0355f81549be54a8
KanonM2014 <shaka.pempek@gmail.com>
2025-12-24 14:21:41 +0700

    update

--- apipipiipiiipppipipipiipipipipipipip/Permainan.py
+++ apipipiipiiipppipipipiipipipipipipip/Permainan.py
@@ -1,20 +1,29 @@
 import random
 from fastapi import APIRouter
-from Pemainininininininininininin import Pemain
-
+from .Pemainininininininininininin import Pemain
+from .Pertanyaaan import Nanya
+from .Tantangan import Perintah
 Router=APIRouter()
 
 @Router.get("/Acak-acakan")
 def Random():
     try:
         Sesuai=random.choice(Pemain)
-        return f"{Sesuai}"
+        return f"{Sesuai}."
     except:
         return"Tidak ada pemainnya!"
     
-
-
 @Router.get("/Acak-TruthorDare")
 def Acak():
     TruthOrDare=random.choice(["Truth","Dare"])
-    return f"Kamu mendapatkan {TruthOrDare}"
+    return f"Kamu mendapatkan {TruthOrDare}."
+
+@Router.get("/Acak Pertanyaan")
+def Putar():
+    Pertanyaan=random.choice(Nanya)
+    return f"Kamu mendapatkan {Pertanyaan}."
+
+@Router.get("/Acak Tantangan")
+def Around():
+    Tantangan=random.choice(Perintah)
+    return f"Kamu mendapatkan {Tantangan}."
